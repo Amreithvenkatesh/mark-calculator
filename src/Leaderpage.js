c16e12b7574f3733fc39d6ef810e802da7a94b69
Amreith <72003702+Amreithvenkatesh@users.noreply.github.com>
2021-03-04 18:29:04 +0530

    Add files via upload

---
+++ src/Leaderpage.js
@@ -0,0 +1,38 @@
+import {useState,useEffect} from 'react';
+import Leaderboard from './Leaderboard';
+
+const Leader_page = () => {
+    const [marklist,setMarklist] = useState(null);
+    const [isLoading,setisLoading] = useState(true);
+    const [error, setError] = useState(null);
+    
+        useEffect(()=>{
+            fetch('https://mark-calculator.herokuapp.com/leaderboard')
+                .then(res =>{
+                    if(!res.ok){
+                        throw Error('Data not fetched');
+                    }
+                    return res.json();
+                })
+                .then(data =>{  
+                    setMarklist(data);
+                    setisLoading(false);
+                    setError(null); 
+                })
+                .catch(err =>{
+                    setError(err.messsage);
+                    setisLoading(false);
+                })
+        },[]
+        );
+
+    return ( 
+        <div className="leader-page">
+            {error && <div>{error}</div>}
+            {isLoading && <div>Fetching data...</div>}
+            {marklist && <Leaderboard marklist={marklist} />}
+        </div>
+     );
+}
+ 
+export default Leader_page;
